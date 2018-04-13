46152b2d10a1fe5e3769cd9efe58ded7a76ee5cb
b-galazka <bartlomiejgalazka@zoho.eu>
2018-04-13 14:23:43 +0200

    add images resizing

--- config.js
+++ config.js
@@ -18,8 +18,47 @@ const {
     DOMAIN,
     UPLOADS_DIR,
     MAX_UPLOADED_FILE_SIZE,
-    FILE_UPLOAD_TIMEOUT
+    FILE_UPLOAD_TIMEOUT,
+    IMAGES_ICONS_DIMENSIONS,
+    RESIZED_IMAGES_DIMENSIONS
 } = process.env;
+
+let imagesIconsDimensions;
+let resizedImagesDimensions;
+
+if (IMAGES_ICONS_DIMENSIONS) {
+
+    const [width, height] = IMAGES_ICONS_DIMENSIONS.split(',');
+
+    imagesIconsDimensions = {
+        width: +width,
+        height: +height
+    };
+
+} else {
+
+    imagesIconsDimensions = {
+        width: 200,
+        height: 200
+    };
+}
+
+if (RESIZED_IMAGES_DIMENSIONS) {
+
+    const [width, height] = RESIZED_IMAGES_DIMENSIONS.split(',');
+
+    resizedImagesDimensions = {
+        width: +width,
+        height: +height
+    };
+
+} else {
+
+    resizedImagesDimensions = {
+        width: 600,
+        height: 600
+    };
+}
 
 module.exports = {
 
@@ -40,5 +79,8 @@ module.exports = {
 
     uploadsDir: path.resolve(UPLOADS_DIR || './uploaded_files'),
     maxUploadedFileSize: +MAX_UPLOADED_FILE_SIZE || 10485760,
-    fileUploadTimeout: +FILE_UPLOAD_TIMEOUT || 60000
+    fileUploadTimeout: +FILE_UPLOAD_TIMEOUT || 60000,
+
+    imagesIconsDimensions,
+    resizedImagesDimensions
 };
---
+++ tools/ImageResizer.js
@@ -0,0 +1,56 @@
+const cuid = require('cuid');
+const path = require('path');
+const sharp = require('sharp');
+
+const {
+    uploadsDir,
+    resizedImagesDimensions,
+    imagesIconsDimensions
+} = require('../config');
+
+class ImageResizer {
+
+    constructor(filePath) {
+
+        this._filePath = filePath;
+        this._extension = filePath.slice(filePath.lastIndexOf('.'));
+    }
+
+    createIcon() {
+
+        return (async () => {
+
+            const path = this._createRandomPath();
+            const { width, height } = imagesIconsDimensions;
+
+            await sharp(this._filePath)
+                .resize(width, height)
+                .toFile(path);
+
+            return path;
+        })();
+    }
+
+    createResizedImage() {
+
+        return (async () => {
+
+            const path = this._createRandomPath();
+            const { width, height } = resizedImagesDimensions;
+
+            await sharp(this._filePath)
+                .resize(width, height)
+                .min()
+                .toFile(path);
+
+            return path;
+        })();
+    }
+
+    _createRandomPath() {
+
+        return path.resolve(uploadsDir, cuid() + this._extension);
+    }
+}
+
+module.exports = ImageResizer;
