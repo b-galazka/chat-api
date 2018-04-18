afa5d842268858d87bfbf2b8986b64ca4f005d7f
b-galazka <bartlomiejgalazka@zoho.eu>
2018-04-18 12:22:38 +0200

    improve dropping DB script

--- db/drop.js
+++ db/drop.js
@@ -4,9 +4,9 @@ const { dbName, dbHost, dbPassword, dbUser } = require('../config');
 
 (async () => {
 
-    try {
+    const { NODE_ENV } = process.env;
 
-        const { NODE_ENV } = process.env;
+    try {
 
         const connection = await mysql.createConnection({
             host: dbHost,
@@ -18,11 +18,11 @@ const { dbName, dbHost, dbPassword, dbUser } = require('../config');
 
         await connection.destroy();
 
-        console.log(`${NODE_ENV} DB dropped`);
+        console.log(`${NODE_ENV || ''} DB dropped`);
 
     } catch (err) {
 
-        console.error(`something went wrong during dropping ${NODE_ENV} DB`);
+        console.error(`something went wrong during dropping ${NODE_ENV || ''} DB`);
         console.error(err);
     }
 })();
